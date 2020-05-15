1b95b5f60dc3af3c936bea7fec40791e28c555c1
Ritik Chauhan <itzhrithan@gmail.com>
2020-05-15 18:49:45 +0530

    Updates

---
+++ precache-manifest.6d0b80bf1a76be6e2560478ffbe04ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318454c994b206053b8306e0e603add2",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "6d2bf4b1921bd5408990",
+    "url": "/pizza/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "da14d63467d24478f743",
+    "url": "/pizza/static/css/main.3a834006.chunk.css"
+  },
+  {
+    "revision": "6d2bf4b1921bd5408990",
+    "url": "/pizza/static/js/2.5b107d05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza/static/js/2.5b107d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da14d63467d24478f743",
+    "url": "/pizza/static/js/main.ad09e80a.chunk.js"
+  },
+  {
+    "revision": "99e2087ef561d78aed87",
+    "url": "/pizza/static/js/runtime-main.be8d9253.js"
+  }
+]);
